d1cda294e345a67374700c82ea04060bdd5940c3
Igor Smolkov <smolkov.igor@gmail.com>
2024-03-14 19:46:00 +0300

    correct grid

--- components/Catalog.tsx
+++ components/Catalog.tsx
@@ -5,9 +5,9 @@ const Catalog: FC = () => {
   const imgKeys = Array.from({ length: 27 }, (_, i) => i);
 
   return (
-    <div className="flex flex-wrap gap-4 p-2">
+    <div className="flex flex-wrap gap-4 p-4">
       {imgKeys.map((index) => (
-        <div key={index} className="aspect-square w-32 rounded-md overflow-hidden shadow-xl">
+        <div key={index} className="aspect-square w-24 rounded-md overflow-hidden shadow-xl">
           <Image className="object-cover h-full" src={`/images/${index}.jpg`} alt="" width={128} height={128} />
         </div>
       ))}
